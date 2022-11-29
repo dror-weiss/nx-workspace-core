d0f3f53a8ac835ec3753a6b0f8b3ac0dd5935e60
dror <dror.weiss.85@gmail.com>
2022-11-29 21:23:41 +0200

    jjj

--- packages/ts-library/src/lib/ts-library.ts
+++ packages/ts-library/src/lib/ts-library.ts
@@ -7,3 +7,13 @@
 export function isNull<T>(val: T | null): val is null {
   return val === null;
 }
+
+/**
+ * Check if the passed value is null.
+ *
+ * @param val
+ * @returns `true` if val is null, `false` otherwise.
+ */
+export function isNotNull<T>(val: T | null) {
+  return val !== null;
+}
